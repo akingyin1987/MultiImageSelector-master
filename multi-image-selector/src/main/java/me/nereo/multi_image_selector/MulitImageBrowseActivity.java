98eafdb64db5e77e7d5e868737912ebb5ec2d5f6
akingyin1987 <akingyin@163.com>
2016-01-05 18:11:09 +0800

    Initial commit

---
+++ multi-image-selector/src/main/java/me/nereo/multi_image_selector/MulitImageBrowseActivity.java
@@ -0,0 +1,106 @@
+package me.nereo.multi_image_selector;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.ViewPager;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.nereo.multi_image_selector.adapter.ImageViewPageAdapter;
+
+import me.nereo.multi_image_selector.bean.Image;
+
+/**
+ * Created by zlcd on 2016/1/5.
+ */
+public class MulitImageBrowseActivity  extends FragmentActivity {
+
+    private boolean hasFolderGened = false;
+    private boolean mIsShowCamera = false;
+
+    // 不同loader定义
+    private static final int LOADER_ALL = 0;
+    private static final int LOADER_CATEGORY = 1;
+
+    public List<Image>   imageList = new ArrayList<>();
+
+    public ImageViewPageAdapter   adapter;
+
+    public ViewPager    viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewpage_select);
+        viewPager = (ViewPager)findViewById(R.id.view_pager);
+        adapter=new ImageViewPageAdapter(this,null);
+        viewPager.setAdapter(adapter);
+        getSupportLoaderManager().initLoader(LOADER_ALL, null, mLoaderCallback);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private LoaderManager.LoaderCallbacks<Cursor> mLoaderCallback = new LoaderManager.LoaderCallbacks<Cursor>() {
+
+        private final String[] IMAGE_PROJECTION = {
+            MediaStore.Images.Media.DATA,
+            MediaStore.Images.Media.DISPLAY_NAME,
+            MediaStore.Images.Media.DATE_ADDED,
+            MediaStore.Images.Media._ID };
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            if(id == LOADER_ALL) {
+                CursorLoader cursorLoader = new CursorLoader(MulitImageBrowseActivity.this,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_PROJECTION,
+                    null, null, IMAGE_PROJECTION[2] + " DESC");
+                return cursorLoader;
+            }else if(id == LOADER_CATEGORY){
+                CursorLoader cursorLoader = new CursorLoader(MulitImageBrowseActivity.this,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, IMAGE_PROJECTION,
+                    IMAGE_PROJECTION[0]+" like '%"+args.getString("path")+"%'", null, IMAGE_PROJECTION[2] + " DESC");
+                return cursorLoader;
+            }
+
+            return null;
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            if (data != null) {
+                List<Image> images = new ArrayList<>();
+                int count = data.getCount();
+                if (count > 0) {
+                    data.moveToFirst();
+                    do{
+                        String path = data.getString(data.getColumnIndexOrThrow(IMAGE_PROJECTION[0]));
+                        String name = data.getString(data.getColumnIndexOrThrow(IMAGE_PROJECTION[1]));
+                        long dateTime = data.getLong(data.getColumnIndexOrThrow(IMAGE_PROJECTION[2]));
+                        Image image = new Image(path, name, dateTime);
+                        images.add(image);
+                    }while(data.moveToNext());
+
+                    adapter.setData(images);
+                    hasFolderGened = true;
+
+                }
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+
+        }
+    };
+}
---
+++ multi-image-selector/src/main/java/me/nereo/multi_image_selector/adapter/ImageViewPageAdapter.java
@@ -0,0 +1,103 @@
+package me.nereo.multi_image_selector.adapter;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import me.nereo.multi_image_selector.R;
+import me.nereo.multi_image_selector.bean.Image;
+
+
+/**
+ * Created by zlcd on 2016/1/5.
+ */
+public class ImageViewPageAdapter  extends PagerAdapter {
+
+    public   Context   context;
+
+    public ViewPager   viewPager;
+
+    public List<Image>   images = new ArrayList<>();
+
+    public   ViewHolder[]  mImageViews  = new ViewHolder[4];
+
+    public ImageViewPageAdapter(Context context,List<Image> images) {
+        this.context = context;
+        if(null != images){
+            this.images = images;
+        }
+
+
+
+    }
+
+    public   void  setData(List<Image>  images){
+        this.images = images;
+        notifyDataSetChanged();
+    }
+
+
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ViewHolder   holder = mImageViews[position % mImageViews.length];
+        if(null != holder){
+            container.removeView(holder.view);
+        }
+
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ViewHolder  holde = mImageViews[position % mImageViews.length];
+        Image  image = images.get(position);
+        if(null == holde){
+            View   view = LayoutInflater.from(context).inflate(R.layout.list_item_bigimage,null);
+            holde = new ViewHolder();
+            holde.view = view;
+            holde.imageView=(ImageView)view.findViewById(R.id.image);
+            ((ViewPager)container).addView(view, 0);
+            mImageViews[position % mImageViews.length]=holde;
+        }
+        Picasso.with(context)
+            .load(new File(image.path))
+            .placeholder(R.drawable.default_error)
+            .error(R.drawable.default_error)
+
+            .into(holde.imageView);
+        return  holde;
+    }
+
+
+
+    public   static  class  ViewHolder{
+
+        View    view;
+
+        CheckBox   checkBox;
+
+        ImageView   imageView;
+    }
+}
